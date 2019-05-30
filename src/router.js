488770fe6661515accf8df2dc2245564785a40fe
Omar Gutierrez Condori <omargc.mus@gmail.com>
2019-05-30 18:57:17 -0400

    total

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Total from './views/Total.vue'
 
 Vue.use(Router)
 
@@ -14,10 +15,7 @@ export default new Router({
     {
       path: '/respuesta/:key',
       name: 'respuesta',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Total.vue')
+      component: Total
     }
   ]
 })
